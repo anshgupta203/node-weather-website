ec888cbc7f85d16c5a1a8aa66b16fd1b80370c16
anshgupta203 <56402745+anshgupta203@users.noreply.github.com>
2021-05-29 14:38:14 +0530

    Humidity forecast added

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -10,8 +10,7 @@ const forecast = (latitude, longitude, callback) => {
         }else if(body.error){
             callback('Unable to find location', undefined)
         }else{
-            callback(undefined, body.current.weather_descriptions[0] + `. The temperature is ${body.current.temperature} degrees out`+ ` but it feels like ${body.current.feelslike} degrees` )
-
+            callback(undefined, body.current.weather_descriptions[0] + '. The temperature is ' + body.current.temperature + ' degrees out but it feels like ' + body.current.feelslike + ' degrees.' + ' Humidity ' + body.current.humidity)
         }
     })
 }
